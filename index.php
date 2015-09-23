26bf3b00ee2e975e59ea6d13d62ba6a7f136d0ac
Graham O'Grady <Graham_OGrady@outlook.com>
2015-09-23 18:08:33 -0400

    Index.php added

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+?>
+<html>
+<header>
+</header>
+<body>
+<img src="http://www.bankofcanada.ca/wp-content/uploads/2015/09/careers-3.jpg">
+</body>
+<footer>
+~~~~~~~~~~~~~~~~~~~~~~~~~~~
+</footer>
+</html>
